53d5f40579c9a774023fa8ed25d133b19e113883
blessed <sibandablessed724@gmail.com>
2023-11-17 07:28:43 +0000

    start

---
+++ fitness/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import { StickyNavbar } from './components/Navbar'
+
+
+export default function Home() {
+  return (
+    <div className=''>
+<StickyNavbar/>
+    </div>
+  )
+}
